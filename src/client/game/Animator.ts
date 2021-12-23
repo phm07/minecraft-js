9b1b7bc608ad01ca341e660a1d3ca9be3f6620fe
pauhull <pauhull3@gmail.com>
2021-12-23 12:53:47 +0100

    Body yaw

---
+++ src/client/game/Animator.ts
@@ -0,0 +1,44 @@
+import Interpolator from "../../common/Interpolator";
+import Util from "../../common/Util";
+import Humanoid from "../models/Humanoid";
+import Human from "./Human";
+
+class Animator {
+
+    private readonly human: Human;
+    private readonly model: Humanoid;
+    private readonly interpolator: Interpolator;
+    private swingTime: number;
+
+    public constructor(human: Human, model: Humanoid) {
+        this.human = human;
+        this.model = model;
+        this.interpolator = new Interpolator({ walk: 0, swingSpeed: 0, bodyYaw: 0 });
+        this.swingTime = 0;
+    }
+
+    public update(delta: number): void {
+
+        const speed = Util.dist(this.human.velocity.x, this.human.velocity.z, 0, 0);
+        const walking = this.human.velocity.y >= -10 && speed > 10e-8;
+
+        this.interpolator.update(delta);
+        this.interpolator.animate("swingSpeed", speed/3.0, 0.1);
+        if (walking) {
+            this.interpolator.animate("walk", 1, 0.2);
+            this.interpolator.animate("bodyYaw", this.human.position.yaw, 0.1);
+        } else {
+            this.interpolator.animate("walk", 0, 0.2);
+            this.interpolator.animate("bodyYaw", Util.clamp(this.model.bodyYaw, this.human.position.yaw-Math.PI/4, this.human.position.yaw+Math.PI/4), 0.01);
+        }
+
+        const walk = this.interpolator.getValue("walk");
+        const swingSpeed = this.interpolator.getValue("swingSpeed");
+        this.model.bodyYaw = this.interpolator.getValue("bodyYaw");
+
+        this.model.swing = Math.sin(this.swingTime)*(Math.PI/4)*Util.clamp(swingSpeed, 0, 2)*walk;
+        this.swingTime += delta * swingSpeed * walk * 10;
+    }
+}
+
+export default Animator;
--- src/client/game/Human.ts
+++ src/client/game/Human.ts
@@ -4,33 +4,29 @@ import Vec3 from "../../common/Vec3";
 import Shader from "../gl/Shader";
 import Texture from "../gl/Texture";
 import Humanoid from "../models/Humanoid";
-import AnimationManager from "./AnimationManager";
+import Animator from "./Animator";
 
 class Human {
 
     public readonly id: number;
-    public readonly model: Humanoid;
+    public position: PlayerPosition;
     public velocity: Vec3;
-    private readonly animationManager: AnimationManager;
+    private readonly model: Humanoid;
+    private readonly animator: Animator;
     private targetPosition: PlayerPosition;
-    private targetBodyYaw: number;
 
     public constructor(id: number, shader: Shader, texture: Texture, position: PlayerPosition) {
         this.id = id;
+        this.position = PlayerPosition.clone(position);
         this.targetPosition = PlayerPosition.clone(position);
         this.model = new Humanoid(shader, texture, position);
-        this.animationManager = new AnimationManager(this);
+        this.animator = new Animator(this, this.model);
         this.velocity = new Vec3();
-        this.targetBodyYaw = 0;
     }
 
     public setPosition(position: PlayerPosition, velocity: Vec3): void {
-
         this.velocity = velocity;
-        this.animationManager.setWalking(velocity.y >= -10 && Util.distSquare(velocity.x, velocity.z, 0, 0) > 10e-4);
-
         this.targetPosition = position;
-        this.targetBodyYaw = position.yaw;
     }
 
     public delete(): void {
@@ -44,13 +40,13 @@ class Human {
     public update(delta: number): void {
 
         const mix = delta * 25;
-        this.model.position.x = Util.lerp(this.model.position.x, this.targetPosition.x, mix);
-        this.model.position.y = Util.lerp(this.model.position.y, this.targetPosition.y, mix);
-        this.model.position.z = Util.lerp(this.model.position.z, this.targetPosition.z, mix);
-        this.model.position.yaw = Util.lerp(this.model.position.yaw, this.targetPosition.yaw, mix);
-        this.model.position.pitch = Util.lerp(this.model.position.pitch, this.targetPosition.pitch, mix);
-        this.model.bodyYaw = Util.lerp(this.model.bodyYaw, this.targetBodyYaw, mix);
-        this.animationManager.update(delta);
+        this.position.x = Util.lerp(this.position.x, this.targetPosition.x, mix);
+        this.position.y = Util.lerp(this.position.y, this.targetPosition.y, mix);
+        this.position.z = Util.lerp(this.position.z, this.targetPosition.z, mix);
+        this.position.yaw = Util.lerp(this.position.yaw, this.targetPosition.yaw, mix);
+        this.position.pitch = Util.lerp(this.position.pitch, this.targetPosition.pitch, mix);
+        this.animator.update(delta);
+        this.model.position = this.position;
         this.model.update();
     }
 }
